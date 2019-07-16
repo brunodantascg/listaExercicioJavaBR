7399dbd22d3769528331c166f13ace41904aa38d
Bruno Dantas <brunodedantas@gmail.com>
2019-07-16 14:32:44 -0300

    questão 5

---
+++ src/estruturaSequencial1/converteMetrosCentimentros_5.java
@@ -0,0 +1,25 @@
+/***
+ * Questão 5
+ *
+ * Faça um Programa que converta metros para centímetros.
+ *
+ */
+
+import java.util.Scanner;
+
+public class converteMetrosCentimentros_5 {
+    public static void main(String[] args){
+
+        int metros;
+        float cem;
+
+        Scanner entrada = new Scanner(System.in); // Entrada de metros pelo usuário
+        System.out.println("Informe quantos metros: "); // Pedido metros
+        metros = entrada.nextInt();
+
+        cem = metros * 100;
+
+        System.out.println(metros + " m = " + cem + " cm"); // Impressão do resultado
+
+    }
+}
